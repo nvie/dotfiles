dedafcd4e497ad3817499263b5c346063fa04030
Vincent Driessen <vincent@3rdcloud.com>
2014-02-20 10:48:53 +0100

    Ignore non-existing files.

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -96,7 +96,7 @@ end
 
 alias top 'glances'
 function cleanpycs
-	find . -name '*.py[co]' -exec rm '{}' ';'
+	find . -name '*.py[co]' -exec rm -f '{}' ';'
 end
 function cleanorigs
     find . '(' -name '*.orig' -o -name '*.BACKUP.*' -o -name '*.BASE.*' -o -name '*.LOCAL.*' -o -name '*.REMOTE.*' ')' -print0 | xargs -0 rm -f
