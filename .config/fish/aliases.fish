7706279a10a4506fdafd880d073507612915231f
Vincent Driessen <vincent@3rdcloud.com>
2014-09-15 11:13:45 +0200

    Support serving with Python 3.

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -7,7 +7,11 @@ alias et 'vim ~/.tmux.conf'
 
 function serve
 	if test (count $argv) -ge 1
-	    /bin/sh -c "(cd $argv[1] && python -m SimpleHTTPServer)"
+	    if python -c 'import sys; sys.exit(sys.version_info[0] != 3)'
+                /bin/sh -c "(cd $argv[1] && python -m http.server)"
+            else
+                /bin/sh -c "(cd $argv[1] && python -m SimpleHTTPServer)"
+            end
 	else
 	    python -m SimpleHTTPServer
 	end
