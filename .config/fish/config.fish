eada6d1fc19f7b617827c2cfeb9cd42d409bed8f
Vincent Driessen <vincent@3rdcloud.com>
2015-03-12 21:39:10 +0100

    Add Scans to CDPATH.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -149,7 +149,7 @@ if status --is-login
 end
 
 if status --is-interactive
-    set CDPATH . ~/Projects/yesgraph ~/Projects
+    set CDPATH . ~/Projects/yesgraph ~/Projects ~/Dropbox/Scans
 end
 
 # }}}
