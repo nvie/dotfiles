22c61f3a7789e3573dcd929a03ac8abf0df9892d
Vincent Driessen <me@nvie.com>
2015-09-29 12:33:16 +0200

    Fix va and vaa scripts

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -91,15 +91,29 @@ function vfa
 end
 
 function va
-    set ag_pattern (echo "$argv" | sed -Ee 's/[<>]/\\\\b/g')
-    set vim_pattern (echo "$argv" | sed -E -e 's,/,\\\\/,g' -e 's,.*,/\\\\v&,')
-    ag -l --smart-case --null $ag_pattern ^/dev/null | xargs -0 -o vim -c $vim_pattern
+    set pattern $argv[1]
+    if test (count $argv) -gt 1
+        set argv $argv[2..-1]
+    else
+        set argv
+    end
+
+    set ag_pattern (echo $pattern | sed -Ee 's/[<>]/\\\\b/g')
+    set vim_pattern (echo $pattern | sed -E -e 's,([/=]),\\\\\1,g' -e 's,.*,/\\\\v&,')
+    ag -l --smart-case --null $ag_pattern -- $argv ^/dev/null | xargs -0 -o vim -c $vim_pattern
 end
 
 function vaa
-    set ag_pattern (echo "$argv" | sed -Ee 's/[<>]/\\\\b/g')
-    set vim_pattern (echo "$argv" | sed -E -e 's,/,\\\\/,g' -e 's,.*,/\\\\v&,')
-    ag -l --smart-case --null -a $ag_pattern ^/dev/null | xargs -0 -o vim -c $vim_pattern
+    set pattern $argv[1]
+    if test (count $argv) -gt 1
+        set argv $argv[2..-1]
+    else
+        set argv
+    end
+
+    set ag_pattern (echo $argv | sed -Ee 's/[<>]/\\\\b/g')
+    set vim_pattern (echo $argv | sed -E -e 's,([/=]),\\\\\1,g' -e 's,.*,/\\\\v&,')
+    ag -l --smart-case --null -a $ag_pattern -- $argv ^/dev/null | xargs -0 -o vim -c $vim_pattern
 end
 
 function vc
