5c3e29655b06b29081a4887391b555a5a20ba39b
Vincent Driessen <me@nvie.com>
2016-08-01 14:22:28 +0200

    Fix abspath command that's broken since fish==2.3.1

--- .config/fish/functions/abspath.fish
+++ .config/fish/functions/abspath.fish
@@ -1,5 +1,6 @@
 function abspath -d 'Calculates the absolute path for the given path'
     set -l cwd ''
-    pushd $argv[1]; and set cwd (pwd); and popd
+    set -l curr (pwd)
+    cd $argv[1]; and set cwd (pwd); and cd $curr
     echo $cwd
 end
