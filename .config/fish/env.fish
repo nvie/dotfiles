9832636303e74927854c6748670b4044537d34dc
Vincent Driessen <me@nvie.com>
2018-03-16 11:48:05 +0100

    Fix git-toolbelt and Python 2 path config

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -8,6 +8,7 @@ append-to-path /usr/sbin
 append-to-path ~/bin
 append-to-path ~/.local/bin
 append-to-path ~/Projects/syncfrom
+append-to-path ~/Projects/git-toolbelt
 append-to-path /usr/X11R6/bin
 prepend-to-path /usr/local/sbin
 prepend-to-path /usr/local/bin
@@ -17,7 +18,8 @@ prepend-to-path /usr/local/bin
 
 set -gx PIP_DOWNLOAD_CACHE ~/Library/Caches/pip-downloads
 
-# Add more Python versions on the PATH (pypy and python3)
+# Python 2 support
+prepend-to-path (pyenv root)/shims
 
 # Put Postgres.app's command line tools on the PATH
 append-to-path /Applications/Postgres.app/Contents/Versions/9.5/bin
