30f27c65fb66bc4176b5769d13fadd31fb88a114
Vincent Driessen <me@nvie.com>
2020-05-11 09:19:12 +0200

    Use npx instead of Python
    My Python installations have been decaying and managing all of those
    runtimes is... not worth it for this.

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -14,15 +14,7 @@ function pdftext
 end
 
 function serve
-    if test (count $argv) -ge 1
-        if python -c 'import sys; sys.exit(sys.version_info[0] != 3)'
-            /bin/sh -c "(cd $argv[1] && python -m http.server)"
-        else
-            /bin/sh -c "(cd $argv[1] && python -m SimpleHTTPServer)"
-        end
-    else
-        python -m SimpleHTTPServer
-    end
+    npx http-server --port 8000 $argv
 end
 
 function timestamp
