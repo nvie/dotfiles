f088761ec208853fe7daea3f15a78a9dcf03ad6b
Vincent Driessen <me@nvie.com>
2020-05-11 09:19:54 +0200

    Add "val", which is like "va", but only lists files

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -76,6 +76,31 @@ function vf
     f | fzf | xargs -o vim
 end
 
+function val
+    set pattern $argv[1]
+    if test (count $argv) -gt 1
+        set argv $argv[2..-1]
+    else
+        set argv
+    end
+
+    function to_safe
+        sed -E -e 's/[\\][=]/__EQ__/g' -e 's/[\\][<]/__LT__/g' -e 's/[\\][>]/__GT__/g'
+    end
+
+    function to_unsafe_rg
+        sed -E -e 's/__LT__/</g' -e 's/__GT__/>/g' -e 's/__EQ__/=/g'
+    end
+
+    function to_unsafe_vim
+        sed -E -e 's/__LT__/[<]/g' -e 's/__GT__/[>]/g' -e 's/__EQ__/[=]/g'
+    end
+
+    set rg_pattern (echo $pattern | to_safe | sed -E -e 's/[<>]/\\\\b/g' | to_unsafe_rg)
+    set vim_pattern (echo $pattern | to_safe | sed -E -e 's,([/=]),\\\\\1,g' -e 's,.*,/\\\\v&,' | to_unsafe_vim)
+    rg -l --smart-case $rg_pattern -- $argv 2>/dev/null
+end
+
 function va
     set pattern $argv[1]
     if test (count $argv) -gt 1
