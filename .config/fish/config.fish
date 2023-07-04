66b9ea31de56e4cdef7a095a554b11bbccc8180e
Vincent Driessen <me@nvie.com>
2023-07-04 21:42:18 +0200

    pnpm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -174,3 +174,9 @@ end
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# pnpm
+set -gx PNPM_HOME "$HOME/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
