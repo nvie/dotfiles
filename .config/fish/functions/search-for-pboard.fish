9cd8f5a0f47d3f957e695f36096acdc1632da026
Vincent Driessen <me@nvie.com>
2024-02-22 20:22:32 +0100

    Keep search-for-pboard working, even with multi-line pastes

--- .config/fish/functions/search-for-pboard.fish
+++ .config/fish/functions/search-for-pboard.fish
@@ -1,14 +1,12 @@
 function search-for-pboard
-    # Never paste if there are more than 2 lines
-    if test (pbpaste | wc -l) -lt 2
-        commandline -- 'va \''(
-		    pbpaste \
-		    | sed -E \
-		          -e 's/^[[:space:]]*//' \
-		          -e 's/[[:space:]]*$//' \
-		          -e "s/[.]/__DOT__/g" \
-		          -e "s/[]['\"\\`()*{}+^\$?|<>&]/./g" \
-		          -e "s/__DOT__/\[.\]/g" \
-		)'\''
-    end
+    commandline -- 'va \''(
+        pbpaste \
+        | head -n1 \
+        | sed -E \
+              -e 's/^[[:space:]]*//' \
+              -e 's/[[:space:]]*$//' \
+              -e "s/[.]/__DOT__/g" \
+              -e "s/[]['\"\\`()*{}+^\$?|<>&]/./g" \
+              -e "s/__DOT__/\[.\]/g" \
+        )'\''
 end
